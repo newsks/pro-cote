16e72090487ff38fb18f09c2e7880b8a6a2583a2
Frontend <sweetysks23@gmail.com>
2024-01-07 21:45:27 +0900

    힙요소 추가삭제  다시 풀어보기

--- algorithm/08_힙요소추가_로직.js
+++ algorithm/08_힙요소추가_로직.js
@@ -2,30 +2,24 @@ class MaxHeap {
   constructor() {
     this.heap = [null];
   }
-
   push(value) {
     this.heap.push(value);
     let currentIndex = this.heap.length - 1;
     let parentIndex = Math.floor(currentIndex / 2);
-
     while (parentIndex !== 0 && this.heap[parentIndex] < value) {
       const temp = this.heap[parentIndex];
       this.heap[parentIndex] = value;
       this.heap[currentIndex] = temp;
-
       currentIndex = parentIndex;
       parentIndex = Math.floor(currentIndex / 2);
     }
   }
-
   pop() {
     const returnValue = this.heap[1];
     this.heap[1] = this.heap.pop();
-
     let currentIndex = 1;
     let leftIndex = 2;
     let rightIndex = 3;
-
     while (
       this.heap[currentIndex] < this.heap[leftIndex] ||
       this.heap[currentIndex] < this.heap[rightIndex]

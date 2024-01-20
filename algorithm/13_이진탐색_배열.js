b7820627a9aec6f6330d845a69f7c70297072b61
Frontend <sweetysks23@gmail.com>
2024-01-20 11:59:38 +0900

    이진탐색&입국심사 문제풀이

---
+++ algorithm/13_이진탐색_배열.js
@@ -0,0 +1,24 @@
+const array = [1, 1, 5, 124, 400, 500, 1004, 2876, 8712];
+
+function binarySearch(array, findValue) {
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Math.floor((left + right) / 2);
+
+  while (left <= right) {
+    if (array[mid] === findValue) {
+      return mid;
+    }
+
+    if (array[mid] < findValue) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+    mid = Math.floor((right + left) / 2);
+  }
+  return -1;
+}
+console.log(binarySearch(array, 2876));
+console.log(binarySearch(array, 1));
+console.log(binarySearch(array, 500));
---
+++ algorithm/15_이진탐색트리_입국심사.js
@@ -0,0 +1,33 @@
+// 로그 시간 = 이진 탐색
+// times -> 선형 로그 신간으로 충분히 가능!
+
+// 우리는 특정 값을 찾는 것이 아닙니다.
+// 우리가 찾는 것은 최소 몇분에 모든 심사가 끝나는가?
+// ㄴ 결정 문제 = 이진 탐색 = 파라메트릭 서치(Parametric Search)
+
+// 최소 1분에서 10억분 * n 사이
+// 면접관들이 몇 명을 처리하는가?
+// 처리 가능한 입국자 n보다 작다면, 분을 올려야되고, 입국자가 n보다 크다면 분을 낮춰야한다.
+// 면접관이 시간대비 몇 명을 처리할 수 있는가?
+// 시간 / 심사시간 = 심사관 당 처리 가능한 입국자 수
+
+// sortedTimes : 심사하는데 걸리는 시간
+
+function solution(n, times) {
+  const sortedTimes = times.sort((a, b) => a - b); //O(n log n)
+  let left = 1; // 1명을 심사하는데 걸리는 시간은 최소 1분이니깐
+  let right = sortedTimes[sortedTimes.length - 1] * n;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    // sum([시간 / 심사시간])
+    const sum = times.reduce((acc, time) => acc + Math.floor(mid / time), 0);
+
+    if (sum < n) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+}

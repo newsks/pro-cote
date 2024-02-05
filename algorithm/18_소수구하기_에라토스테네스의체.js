0c3bd9cdae706ca0602af0872e8a588cf99f2bda
Frontend <sweetysks23@gmail.com>
2024-02-05 21:55:06 +0900

    소수구하기

---
+++ algorithm/18_소수구하기_에라토스테네스의체.js
@@ -0,0 +1,12 @@
+function get_primes(num) {
+  const prime = [false, false, ...Array(num - 1).fill(true)];
+
+  for (let i = 2; i * i <= num; i += 1) {
+    if (prime[i]) {
+      for (let j = i * 2; j <= num; j += 1) {
+        prime[j] = false;
+      }
+    }
+  }
+  return prime.filter(Boolean);
+}

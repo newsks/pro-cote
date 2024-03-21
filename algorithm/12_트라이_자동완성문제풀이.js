1d18a59a94cfadbb1b9948f4feb34794cb180fc6
Frontend <sweetysks23@gmail.com>
2024-03-21 22:33:18 +0900

    트라이,이진탐색 복습

--- algorithm/12_트라이_자동완성문제풀이.js
+++ algorithm/12_트라이_자동완성문제풀이.js
@@ -19,7 +19,6 @@
 
 function makeTrie(words) {
   const root = {};
-
   for (const word of words) {
     let current = root;
     for (const letter of word) {
@@ -31,15 +30,12 @@ function makeTrie(words) {
   }
   return root;
 }
-
 function solution(words) {
   let answer = 0;
   const trie = makeTrie(words);
-
   for (const word of words) {
     let count = 0;
     let current = trie;
-
     for (const [index, letter] of [...word].entries()) {
       count += 1;
       if (current[letter][0] <= 1) {

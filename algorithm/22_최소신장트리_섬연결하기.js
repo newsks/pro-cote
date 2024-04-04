41f0d16af611b46a4e13188f5144ba2f4f27b0d3
Frontend <sweetysks23@gmail.com>
2024-04-04 17:29:32 +0900

    최소신장트리,투포인터,백트래킹 복습

--- algorithm/22_최소신장트리_섬연결하기.js
+++ algorithm/22_최소신장트리_섬연결하기.js
@@ -24,7 +24,6 @@ function solution(n, costs) {
   let answer = 0;
   let islands = [];
   let bridges = costs.sort((a, b) => a[2] - b[2]);
-
   for (let i = 0; i < n; i++) {
     islands[i] = i;
   }

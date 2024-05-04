55ec2713e405058640d1b2e090c08f533a3ba064
Frontend <sweetysks23@gmail.com>
2024-05-04 22:15:08 +0900

    큐, 해시, BFS 복습

--- algorithm/03_queue_linkedList.js
+++ algorithm/03_queue_linkedList.js
@@ -5,12 +5,14 @@ class Node {
     this.next = null;
   }
 }
+
 class Queue {
   constructor() {
     this.head = null;
     this.tail = null;
     this.size = 0;
   }
+
   enqueue(newValue) {
     const newNode = new Node(newValue);
     if (this.head === null) {
@@ -21,12 +23,14 @@ class Queue {
     }
     this.size += 1;
   }
+
   dequeue() {
     const value = this.head.value;
     this.head = this.head.next;
     this.size -= 1;
     return value;
   }
+  
   peek() {
     return this.head.value;
   }

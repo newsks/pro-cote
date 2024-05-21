535daeb1dfef7fa0a1b5fdbe6f38c0a8f93c6223
Frontend <sweetysks23@gmail.com>
2024-05-21 16:24:29 +0900

    BFS,그리디 복습

--- algorithm/16_BFS,DFS_가장먼노드_shift.js
+++ algorithm/16_BFS,DFS_가장먼노드_shift.js
@@ -30,13 +30,11 @@ function solution(n, edge) {
     graph[src].push(dest); // 출발지(graph[src]) => 도착지(dest)를 인접리스트에 추가
     graph[dest].push(src); // 양방향이기때문에 둘다 구현  도착지 => 출발지
   }
-
   // 각 정점의 길이를 구할 수 있도록 배열 만들기
   const distance = Array(n + 1).fill(0); // 정점만큼 배열을 만들고 0으로 초기화
   distance[1] = 1; // 1번의 길이는 1이라고 정한다.
   // BFS(너비우선탐색) : 간선을 순회하기위해 인접요소부터 탐색
   const queue = [1];
-
   while (queue.length > 0) {
     const src = queue.shift(); // 원점 shift는 O(n)이지만 요소가 적을 경우에는 자바스크립트 엔진에서 최적화를 해준다.
     for (const dest of graph[src]) {
@@ -48,7 +46,6 @@ function solution(n, edge) {
       }
     }
   }
-
   const max = Math.max(...distance); // 거리들 중에 가장 큰 값을 뽑아준다
   return distance.filter((item) => item === max).length; // 최대값과 같은 요소들이 몇개나 있는지 length를 통해 구해준다
 }
